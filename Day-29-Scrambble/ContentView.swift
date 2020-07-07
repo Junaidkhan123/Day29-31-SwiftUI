20712182a9978cbc0795e3182e4c93ed315a63f9
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-07-07 15:31:36 +0500

    Initial Commit

---
+++ Day-29-Scrambble/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Day-29-Scrambble
+//
+//  Created by Jhon Khan on 07/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day-29-Scrambble/Day_29_ScrambbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_29_ScrambbleApp.swift
+//  Day-29-Scrambble
+//
+//  Created by Jhon Khan on 07/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Day_29_ScrambbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
